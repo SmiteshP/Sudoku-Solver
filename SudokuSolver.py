5017758788b0613041f1302843459f4e54164eca
SmiteshP <smiteshpatil2010@gmail.com>
2019-12-21 11:46:09 +0530

    Add intro comments

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,3 +1,38 @@
+'''
+Sudoku-Solver
+	created by - Smitesh V. Patil
+	on - 21, December, 2019
+
+version - 1.0
+
+create the Grid instance by giving it a list of lists, 
+each inner list represents a row of the puzzle
+enter 0 inplace of empty places to be solved
+Example :-
+		Grid([
+				[0, 0, 2,  8, 7, 0,  0, 0, 0],
+				[7, 0, 0,  0, 0, 4,  6, 0, 0],
+				[6, 4, 3,  9, 0, 0,  0, 7, 0],
+
+				[5, 2, 6,  1, 0, 0,  8, 9, 0],
+				[0, 0, 0,  0, 8, 0,  0, 0, 0],
+				[0, 9, 8,  0, 0, 2,  1, 6, 3],
+
+				[0, 1, 0,  0, 0, 7,  9, 3, 2],
+				[0, 0, 4,  3, 0, 0,  0, 0, 5],
+				[0, 0, 0,  0, 9, 8,  4, 0, 0]
+			])
+
+This code can only solve the board if it has a unique solution
+Also the validity of the initial game board is NOT chechked 
+and is assumed to be correct.
+
+No fancy algorithm is used to solve the game board, each 
+tile is individually checked if its number can be deduced 
+over and over again until it the board is solved or no
+further progress can be made. 
+'''
+
 class Grid():
 # Grid holds all the data of the gameboard
 
@@ -96,7 +131,7 @@ class Grid():
 		'''
 
 		self.updateGrid()
-		
+
 		for j, row in enumerate(self.grid):
 			for i, num in enumerate(row):
 				if i%3 == 0 and i != 0:
